886543421bfe67cfc5dec49739dfe267e86293c8
Trần Thanh Nguyên <103997885+dutboy642@users.noreply.github.com>
2023-10-31 19:54:46 +0700

    Nguyen (#39)
    * test2
    
    * Buoi 22 Axios server
    
    * check
    
    * del node module
    
    * Buoi 23
    
    * done
    
    * Done V2
    
    * nguyen Buoi24
    
    * Buoi 24 last
    
    * Buoi 25 v1
    
    * Buoi 26 v1
    
    * Buoi 26 v2
    
    ---------
    
    Co-authored-by: Tran Thanh Nguyen <trannguyen.06042002@gmail.comn>

--- HTML_CSS_Final_Nguyen/Buoi9/script.js
+++ HTML_CSS_Final_Nguyen/Buoi9/script.js
@@ -2,6 +2,7 @@ var searchButton = $("#search-button");
 var searchInput = $("#search-input");
 var chatNowBtn = $("#button-chatNow");
 var shopName = $("#shopName");
+var AccountListName = "AccountListName";
 
 var loggedInAccount = "loggedInAccount";
 var cartList = "cartList";
@@ -111,7 +112,7 @@ var getLoggedInAccount = function () {
   if (localStorage.getItem(loggedInAccount)) {
     var shopName = document.getElementById("ShopName");
     shopName.innerText =
-      "Xin chào " + JSON.parse(localStorage.getItem(loggedInAccount));
+      "Xin chào " + JSON.parse(localStorage.getItem(loggedInAccount)).username;
     loginHref.addClass("d-none");
   } else {
     var shopContainer = $("#shopContainer");
@@ -124,6 +125,48 @@ var LogOut = function () {
   localStorage.removeItem(loggedInAccount);
   localStorage.removeItem(cartList);
   window.location.href = "../Buoi8/home.html";
+};
+
+var  AccountInf= function(){
+  var loggedAccount = JSON.parse(localStorage.getItem(loggedInAccount));
+  document.getElementById("usernameModal").innerText= loggedAccount.username;
+  document.getElementById("PayedOrderModal").innerText= loggedAccount.orderNumber;
+  document.getElementById("AmountOfMoney").innerText= loggedAccount.AmountOfMoney;
+  document.getElementById("depositAmount").value =0;
+
+
+}
+
+var deposit =function(){
+  if(parseInt(document.getElementById("depositAmount").value)>=0){
+    var Account = JSON.parse(localStorage.getItem(loggedInAccount));
+  var data = JSON.parse(localStorage.getItem(AccountListName));
+  Account.AmountOfMoney =parseInt(Account.AmountOfMoney)+ parseInt(document.getElementById("depositAmount").value);
+  for (let i = 0 ; i < data.length ; i++){
+    if(data[i].username == Account.username){
+      data[i].AmountOfMoney =parseInt(data[i].AmountOfMoney) +parseInt(document.getElementById("depositAmount").value);
+    }
+  }
+  localStorage.setItem(AccountListName,JSON.stringify(data));
+  localStorage.setItem(loggedInAccount,JSON.stringify(Account));
+  showNotification("Nạp tiền thành công");
+  AccountInf();
+  }
+  else{
+    showNotification("Số tiền không được âm")
+  }
+}
+
+var showNotification = function (mess) {
+  setTimeout(function () {
+    notification.innerText = mess;
+    notification.classList.remove("d-none");
+    notification.style.opacity = "1";
+  }, 100);
+  setTimeout(function () {
+    notification.classList.add("d-none");
+    notification.style.opacity = "0";
+  }, 4000);
 };
 
 getLoggedInAccount();
