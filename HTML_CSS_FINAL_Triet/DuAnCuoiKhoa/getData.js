34cfd89f45a26c2ad0508c342fe5f760727d3c5d
Phạm Minh Triết <142817355+trietpham252@users.noreply.github.com>
2023-11-03 20:42:26 +0700

    update

--- HTML_CSS_FINAL_Triet/DuAnCuoiKhoa/getData.js
+++ HTML_CSS_FINAL_Triet/DuAnCuoiKhoa/getData.js
@@ -7,21 +7,16 @@ document.getElementById("addtocart").addEventListener("click", function () {
     if (product) {
       const quantity = parseInt(document.getElementById("quantity").value); 
 
-      // Kiểm tra số lượng hợp lệ
       if (isNaN(quantity) || quantity <= 0) {
         alert("Số lượng không hợp lệ!");
         return;
       }
 
-      // Lấy danh sách giỏ hàng từ local storage (nếu tồn tại)
       let cartItems = JSON.parse(localStorage.getItem("cart")) || [];
       const existProduct = cartItems.find(x => x.id.toString() === productId)
-      // Kiểm tra xem sản phẩm đã tồn tại trong giỏ hàng chưa
       if (existProduct) {
-        // Sản phẩm đã tồn tại trong giỏ hàng, cộng thêm số lượng
         existProduct.quantity += quantity;
       } else {
-        // Sản phẩm chưa tồn tại trong giỏ hàng, thêm mới vào danh sách
         cartItems.push({
           id: productId,
           name: product.name,
