87a1348579db61bcdb698d9a00d784a3079169d6
Trieu <nhtrieu2k5@gmail.com>
2023-11-22 15:03:29 +0700

    Dang ky Dang nhap

---
+++ HTML_CSS_Final_Trieu/main.js
@@ -0,0 +1,48 @@
+function createUser(username, password) {
+    let accounts = JSON.parse(localStorage.getItem("accounts")) || []
+    const newAccount = {
+        username: username,
+        password: password,
+        fund: 0,
+        orderNumber: 0
+    }
+    accounts.push(newAccount)
+    localStorage.setItem("accounts", JSON.stringify(accounts))
+}
+
+document.getElementById("signupForm").addEventListener("submit", function(event){
+    event.preventDefault()
+    const username = document.getElementById("signupUsername").value
+    const password = document.getElementById("signupPassword").value
+    const confirmPassword = document.getElementById("signupRePassword").value
+
+    if(password !== confirmPassword) {
+        alert("Mật khẩu và mật khẩu nhập lại không trùng khớp.")
+    } else{
+        let accounts = JSON.parse(localStorage.getItem("accounts")) || []
+        if(accounts.find(x => x.username === username)) {
+            alert("Tài khoản đã tồn tại> Vui lòng chọn tài khoản khác.")
+        } else {
+            createUser(username, password)
+            alert("Đăng ký thành công")
+            document.getElementById("signupUsername").value = "";
+            document.getElementById("signupPassword").value = "";
+            document.getElementById("signupRePassword").value = "";
+        }
+    }
+})
+
+
+
+document.getElementById("loginForm").addEventListener("submit", function(event){
+    event.preventDefault()
+    const username = document.getElementById("loginUsername").value
+    const password = document.getElementById("loginPassword").value
+    let accounts = JSON.parse(localStorage.getItem("accounts")) || []
+    if(accounts.find(x => x.password === password)) {
+        alert("Đăng nhập thành công")
+        window.location.href = "home.html"      
+    } else {  
+        alert("Đăng nhập không thành công. Vui lòng kiểm tra tài khoản và mật khẩu.")
+    }
+})
